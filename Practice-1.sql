ee267ea0c2f2e71a8212ca6e4eb4fc0252d9c2aa
Akki2k23 <akashakki992@gmail.com>
2023-07-10 12:41:14 +0000

    commit

---
+++ Practice-1.sql
@@ -0,0 +1,12 @@
+--to know the details of table
+desc HR_DEPARTMENT;
+
+/*ddl--->create,alter,drop,truncate,rename,comment
+dml--->insert,update,delete,select,merge,call,explain plan,lock teble
+dcl--->grant,revoke
+tcl--->commit,savepoint,rollback,set transaction/*
+
+
+
+
+
